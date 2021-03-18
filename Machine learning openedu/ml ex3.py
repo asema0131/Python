542e1495c13c99c150ff57e38cb6142e8e4cf9f2
asem.nurzhanova <asem.nurzhanova@outlook.com>
2021-03-18 20:38:54 +0600

    3

--- Machine learning openedu/ml ex3.py
+++ Machine learning openedu/ml ex3.py
@@ -1,15 +1,15 @@
 print('ML ex 3')
 import numpy as np
 import pandas as pd
-data = pd.read_csv("candy-test1.csv", encoding = 'utf-8', delimiter = ',', index_col='competitorname')
-data_test = pd.read_csv("candy-test1.csv", encoding = 'utf-8', delimiter = ',', index_col='competitorname')
+data = pd.read_csv("data1.csv",  delimiter = ',', index_col='competitorname')
+data_test = pd.read_csv("test1.csv", delimiter = ',', index_col='competitorname')
 b = data_test.index.get_loc('Tootsie Roll Midgies')
 p = data_test.index.get_loc('Swedish Fish')
 print(b,p)
 bb = data_test.iloc[b]
 Y1 = bb.drop(['Y'])
 pp = data_test.iloc[p]
-Y2 = pp.drop(['Y'])
+Y2 = pp.drsop(['Y'])
 train_data = data.drop(['One dime','Fun Dip, Milky Way'])
 X = pd.DataFrame(train_data.drop(['winpercent', 'Y'], axis=1))
 Y = pd.DataFrame(train_data['Y'])
