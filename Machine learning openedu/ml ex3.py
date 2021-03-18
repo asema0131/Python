1849165d291a40642e0ce5461aa42f2a3f100f0f
asem.nurzhanova <asem.nurzhanova@outlook.com>
2021-03-18 13:24:25 +0600

    2

---
+++ Machine learning openedu/ml ex3.py
@@ -0,0 +1,20 @@
+print('ML ex 3')
+import numpy as np
+import pandas as pd
+data = pd.read_csv("candy-test1.csv", encoding = 'utf-8', delimiter = ',', index_col='competitorname')
+data_test = pd.read_csv("candy-test1.csv", encoding = 'utf-8', delimiter = ',', index_col='competitorname')
+b = data_test.index.get_loc('Tootsie Roll Midgies')
+p = data_test.index.get_loc('Swedish Fish')
+print(b,p)
+bb = data_test.iloc[b]
+Y1 = bb.drop(['Y'])
+pp = data_test.iloc[p]
+Y2 = pp.drop(['Y'])
+train_data = data.drop(['One dime','Fun Dip, Milky Way'])
+X = pd.DataFrame(train_data.drop(['winpercent', 'Y'], axis=1))
+Y = pd.DataFrame(train_data['Y'])
+from sklearn.linear_model import LogisticRegression
+reg = LogisticRegression(random_state=2019, solver='lbfgs').fit(X, Y.values.ravel())
+ProbPredictionY1 = reg.predict_proba([Y1])
+print(ProbPredictionY1)
+ProbPredictionY2 = reg.predict_proba([Y2])
